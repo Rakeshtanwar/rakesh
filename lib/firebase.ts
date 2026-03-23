a6ae37683549cc8ced823be9578d4f3d4aab304f
Rakeshtanwar <117805175+Rakeshtanwar@users.noreply.github.com>
2026-03-23 10:32:35 +0530

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCEidvg8i6mVzLJFm8nbDp6WQl6cqLhhVM",
-  authDomain: "chronos-app-15378.firebaseapp.com",
-  projectId: "chronos-app-15378",
-  storageBucket: "chronos-app-15378.firebasestorage.app",
-  messagingSenderId: "352478911502",
-  appId: "1:352478911502:web:bc94a675c8333779fd305f",
-  measurementId: "G-FBH76HEMTQ"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
